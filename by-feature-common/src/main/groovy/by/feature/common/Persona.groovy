eb4335f3434680685eddd322b44102db25d73eab
heldermorais <helder.morais@gmail.com>
2022-06-10 15:49:09 -0300

    tweaking documentation (Orchid.Run)

--- by-feature-common/src/main/groovy/by/feature/common/Persona.groovy
+++ by-feature-common/src/main/groovy/by/feature/common/Persona.groovy
@@ -3,13 +3,20 @@ package by.feature.common
 import grails.persistence.Entity
 
 /**
- * Simple domain object representing a person.
+ * Simple <b>domain</b> object representing a person.
  *
  * @author Graeme Rocher
  */
 abstract class Persona {
 
+	/**
+	 * atributo que contém o <i>"Nome"</i> de uma pessoa.
+	 */
 	String firstName
+
+	/**
+	 * atributo que contém o <i>"SobrenomeNome"</i> de uma pessoa.
+	 */
 	String lastName
 
 	static constraints = {
